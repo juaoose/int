63434d38346d2a41ccf8bd2df047c72e95cbb816
Juan <juaoose@users.noreply.github.com>
2023-07-24 18:55:28 -0500

    simplify lexer test

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -5,78 +5,6 @@ import (
 	"testing"
 )
 
-func TestNextToken_Dumb(t *testing.T) {
-	input := `=+(){},;`
-
-	tests := []struct {
-		expectedType    token.TokenType
-		expectedLiteral string
-	}{
-		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
-		{token.LPAREN, "("},
-		{token.RPAREN, ")"},
-		{token.LBRACE, "{"},
-		{token.RBRACE, "}"},
-		{token.COMMA, ","},
-		{token.SEMICOLON, ";"},
-	}
-
-	l := New(input)
-
-	for i, tt := range tests {
-		token := l.NextToken()
-
-		if token.Type != tt.expectedType {
-			t.Fatalf("tests[%d] - wrong token type. expected=%q, got=%q", i, tt.expectedType, token.Type)
-		}
-
-		if token.Literal != tt.expectedLiteral {
-			t.Fatalf("tests[%d] - wrong token literal. expected=%q, got=%q", i, tt.expectedLiteral, token.Literal)
-		}
-	}
-}
-
-func TestNextToken_SingleChar(t *testing.T) {
-	input := `!-/*5;
-	5 < 10 > 5;`
-
-	tests := []struct {
-		expectedType    token.TokenType
-		expectedLiteral string
-	}{
-		//!-/*5;
-		{token.BANG, "!"},
-		{token.MINUS, "-"},
-		{token.SLASH, "/"},
-		{token.ASTERISK, "*"},
-		{token.INT, "5"},
-		{token.SEMICOLON, ";"},
-		//5 < 10 > 5;
-		{token.INT, "5"},
-		{token.LT, "<"},
-		{token.INT, "10"},
-		{token.GT, ">"},
-		{token.INT, "5"},
-		{token.SEMICOLON, ";"},
-		{token.EOF, ""},
-	}
-
-	l := New(input)
-
-	for i, tt := range tests {
-		token := l.NextToken()
-
-		if token.Type != tt.expectedType {
-			t.Fatalf("tests[%d] - wrong token type. expected=%q, got=%q", i, tt.expectedType, token.Type)
-		}
-
-		if token.Literal != tt.expectedLiteral {
-			t.Fatalf("tests[%d] - wrong token literal. expected=%q, got=%q", i, tt.expectedLiteral, token.Literal)
-		}
-	}
-}
-
 func TestNextToken_Base(t *testing.T) {
 	input := `let five = 5;
 let ten = 10;
@@ -85,7 +13,9 @@ let add = fn(x, y) {
   x + y;
 };
 
-let result = add(five, ten);`
+let result = add(five, ten);
+!-/*5;
+5 < 10 > 5;`
 
 	tests := []struct {
 		expectedType    token.TokenType
@@ -132,6 +62,20 @@ let result = add(five, ten);`
 		{token.COMMA, ","},
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		//!-/*5;
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		//5 < 10 > 5;
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
